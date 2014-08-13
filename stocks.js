1cc6c3eb22df6c6ec124d716867a8d2c0edeb868
chinenyem nwadiugwu <chinenyemnwadiugwu@chinenyems-MacBook-Air.local>
2014-08-13 08:09:18 -0400

    test constructor

---
+++ stocks.js
@@ -0,0 +1,21 @@
+// function Store(name, tops, bottoms){
+// 	this.name = name;
+// 	this.tops = tops;
+// 	this.bottoms = bottoms;
+// }
+
+// var Lily = new Managereval("idiot", "incorrect")
+// var James = new Managereval("intelligent", "correct")
+
+
+
+
+// function People (name, age) {
+// 	this.name = name;
+// 	this.age = age;
+// 	this ageify = function (increment){
+// 		this.age =
+// 	}
+// }
+
+
